945ec32a939079143450ecedf8904e992b386c45
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-07-16 09:34:46 +0100

    Feedback UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,25 +5,25 @@ function Navbar() {
     return(
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><i class="fas fa-award"></i></a>
+  <a class="navbar-brand" href="/"><i class="fas fa-award"></i></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#"><b>6 Suggestions </b><span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/"><b>6 Suggestions </b><span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item sort">
-        <a class="nav-link" href="#">Sort by:</a>
+        <a class="nav-link" href="/">Sort by:</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown"  aria-expanded="false">
           Most Upvotes
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">A Dropdown!</a>
-          <a class="dropdown-item" href="#">I Presume</a>
+          <a class="dropdown-item" href="/">A Dropdown!</a>
+          <a class="dropdown-item" href="/">I Presume</a>
         </div>
       </li>
     </ul>
